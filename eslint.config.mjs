4673b44c5863ba581b8df86c4d1293a7a14919c8
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-19 02:25:57 +0600

    configured eslint and prettier with nodemon and tsconfig

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import globals from 'globals';
+import jsConfig from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        process: 'readonly',
+      },
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  jsConfig.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+      prettier,
+    },
+    rules: {
+      ...tsEslintPlugin.configs.recommended.rules,
+      ...prettierConfig.rules, 
+      'prettier/prettier': 'off',
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      "no-console": ["warn", { "allow": ["warn", "error"] }],
+      'no-undef': 'error',
+      // semi: ["warn", "always"],
+      // '@typescript-eslint/no-unused-vars': 'error',
+      // '@typescript-eslint/no-unused-expressions': 'error',
+      // "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+  {
+    files: ["src/server.ts"],
+    rules: {
+      "no-console": "off",
+    },
+  },
+];
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express from "express";
-import type { Application, Request, Response } from "express";
+import cors from 'cors';
+import express from 'express';
+import type { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_req: Request, res: Response) => {
-	res.status(200).json({ success: true, message: "🏃 Server is Running!" });
+app.get('/', (_req: Request, res: Response) => {
+	res.status(200).json({ success: true, message: '🏃 Server is Running!' });
 });
 
 export default app;
